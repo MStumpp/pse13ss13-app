9ea24dc6bc5f68b3852e4037153f4674140159f5
Ludwig <ludi.biermann@gmail.com>
2013-06-04 19:23:25 +0200

    convert Options in Fragment Content

--- src/gui/options/Options.java
+++ src/gui/options/Options.java
@@ -1,14 +1,40 @@
 package gui.options;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class Options extends PreferenceActivity  {
+public class Options extends Activity  {
 
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.options);
+        super.onCreate(savedInstanceState);
+		
+        this.setContentView(R.layout.options_view);
+
+        Fragment f = new OptionenFragment();
+        FragmentTransaction t = getFragmentManager().beginTransaction();
+        
+        t.replace(R.id.options_view_id, f);
+        t.addToBackStack(null);
+        
+        t.commit();
+        
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new OptionenFragment()).commit();
 	}
+	public static class OptionenFragment extends PreferenceFragment {
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			Log.d("Options",
+					"Optionen.xml wird als default Preference Manager ausgeführt.");
+			PreferenceManager.setDefaultValues(getActivity(), R.xml.options, false);
+			Log.d("Options", "Optionen werden gestartet.");
+			addPreferencesFromResource(R.xml.options);
+		}
+	}
+
 }
