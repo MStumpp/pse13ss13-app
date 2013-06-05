be3f8d1e3234eea4ae12d64e74eb788bb0418a6e
Ludwig <ludi.biermann@gmail.com>
2013-06-05 12:54:55 +0200

    add java doc

--- src/gui/options/Options.java
+++ src/gui/options/Options.java
@@ -8,33 +8,61 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class Options extends Activity  {
+/**
+ * Ein Controller, der sich um die Anzeige und Verarbeitung des Optionen Menüs kümmert.
+ * 
+ * Diese Klasse nutzt folgende xml Files:
+ * #layout.options_view - Das Grundlayout des Optionen Menüs.
+ * 
+ * Die hier gespeicherten und veränderten Informationen kann man mithilfe dieses Befehls abrufen:
+ * 
+ * import android.preference.PreferenceManager;
+ * SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+ * 
+ * @author Ludwig Biermann
+ * 
+ */
+public class Options extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-		
-        this.setContentView(R.layout.options_view);
+		super.onCreate(savedInstanceState);
 
-        Fragment f = new OptionenFragment();
-        FragmentTransaction t = getFragmentManager().beginTransaction();
-        
-        t.replace(R.id.options_view_id, f);
-        t.addToBackStack(null);
-        
-        t.commit();
-        
-        //getFragmentManager().beginTransaction().replace(android.R.id.content, new OptionenFragment()).commit();
+		this.setContentView(R.layout.options_view);
+
+		Fragment f = new OptionenFragment();
+		FragmentTransaction t = getFragmentManager().beginTransaction();
+
+		t.replace(R.id.options_view_id, f);
+		t.addToBackStack(null);
+
+		t.commit();
 	}
+
+	/**
+	 * Ein Fragment der grapischen Oberfläche, dass das sich um die Anzeige des
+	 * Menüs kümmert.
+	 * 
+	 * Dieses Fragment beöntigt folgende xml Files:
+	 * #xml.optionen - Das xml File aus dem automatisch die Grapische Oberfäche entsteht. 
+	 * #string.options_array - Sprachdatei für Listen
+	 * #string.options - Sprachdatei
+	 * 
+	 * Info: Unter values-de werden die deutschsprachigen Dateien gespeichert.
+	 * 
+	 * @author Ludwig Biermann
+	 * 
+	 */
 	public static class OptionenFragment extends PreferenceFragment {
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			Log.d("Options",
 					"Optionen.xml wird als default Preference Manager ausgeführt.");
-			PreferenceManager.setDefaultValues(getActivity(), R.xml.options, false);
+			
+			PreferenceManager.setDefaultValues(getActivity(), R.xml.options,
+					false);
 			Log.d("Options", "Optionen werden gestartet.");
 			addPreferencesFromResource(R.xml.options);
 		}
 	}
-
 }
