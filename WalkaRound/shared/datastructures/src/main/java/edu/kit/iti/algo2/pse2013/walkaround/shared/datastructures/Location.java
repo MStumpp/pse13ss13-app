e962547f1c8e29c3b4110b5ad78c24e461c43bbf
Thomas Kadow <tkadow@web.de>
2013-06-19 00:34:59 +0200

    fixed package error

--- WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/Location.java
+++ WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/Location.java
@@ -1,4 +1,4 @@
-package walkaround.datastructures;
+package edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures;
 
 public class Location extends Coordinate {
 
--- WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/LocationDataIO.java
+++ WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/LocationDataIO.java
@@ -1,4 +1,4 @@
-package walkaround.datastructures;
+package edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures;
 
 import java.io.File;
 import java.util.ArrayList;
