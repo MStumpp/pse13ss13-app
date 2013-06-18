d7fc32f44effdcb13a3e4a51b99f581c1ab61643
floscher <florian@schaeferban.de>
2013-06-18 14:34:21 +0200

    Introduced Maven folder structure and pom.xml's

---
+++ WalkaRound/client/client-view/src/edu/kit/iti/algo2/pse2013/walkaround/client/view/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.kit.iti.algo2.pse2013.walkaround.client.view;
+
+import edu.kit.iti.algo2.pse2013.walkaround.client.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
