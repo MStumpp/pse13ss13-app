a9d3f75c4d35f06a1a726d47d101cb631112c8e0
Thomas Kadow <tkadow@web.de>
2013-06-19 22:28:40 +0200

    added POIManger and FavoritesManager with unimplemented methods

---
+++ WalkaRound/client/client-model/client-model-data/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/FavoritesManager.java
@@ -0,0 +1,71 @@
+package edu.kit.iti.algo2.pse2013.walkaround.client.model.data;
+
+import java.util.ArrayList;
+
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Location;
+
+public class FavoritesManager {
+
+	private static FavoritesManager instance;
+
+	private ArrayList<RouteInfo> savedRoutes;
+
+	private ArrayList<Location> savedLocations;
+
+	private FavoritesManager() {
+
+	}
+
+	public static FavoritesManager getInstance() {
+		if (instance == null) {
+			instance = new FavoritesManager();
+		}
+		return instance;
+	}
+
+	public ArrayList<RouteInfo> getFavoriteRoutes() {
+		return null;
+	}
+
+	public ArrayList<Location> getFavoriteLocations() {
+		return null;
+	}
+
+	public RouteInfo getFavoriteRoute(int id) {
+		return null;
+	}
+
+	public Location getFavoriteLocation(int index) {
+		return null;
+	}
+
+	// changed boolean return to void
+	public void deleteRoute(int index) {
+
+	}
+
+	// changed boolean return to void
+	public void deleteLocation(int index) {
+
+	}
+
+	// changed boolean return to void
+	public void addRouteToFavorites(RouteInfo routeToSave, String name) {
+
+	}
+
+	// changed boolean return to void
+	public void addLocationToFavorites(Location locationToSave, String name) {
+
+	}
+
+	// changed boolean return to void
+	public void containsRoute(RouteInfo routeInfo) {
+
+	}
+
+	// changed boolean return to void
+	public void containsLocation(Location location) {
+
+	}
+}
---
+++ WalkaRound/client/client-model/client-model-data/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/POIManager.java
@@ -0,0 +1,66 @@
+package edu.kit.iti.algo2.pse2013.walkaround.client.model.data;
+
+import java.util.ArrayList;
+
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Address;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Location;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.LocationDataIO;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.POI;
+
+public class POIManager {
+
+	private static POIManager instance;
+
+	private LocationDataIO locationDataIO;
+
+	private ArrayList<Integer> activeCategories;
+
+	private POIManager(LocationDataIO locationDataIO) {
+		this.locationDataIO = locationDataIO;
+		activeCategories = new ArrayList<Integer>();
+	}
+
+	public static POIManager getInstance(LocationDataIO locationDataIO) {
+		if (instance == null) {
+			instance = new POIManager(locationDataIO);
+		}
+		return instance;
+	}
+
+	public ArrayList<Integer> getActiveCategories() {
+		return activeCategories;
+	}
+
+	// int[] parameter gelöscht da aktive kategorien als attribut vorliegen
+	public ArrayList<POI> getPOIsWithinRectangle(Coordinate upperLeft,
+			Coordinate bottomRight, int levelOfDetail) {
+		return null;
+	}
+
+	// int[] parameter gelöscht da aktive kategorien als attribut vorliegen
+	public ArrayList<POI> getPOIsAlongRoute(RouteInfo routeInfo,
+			int levelOfDetail) {
+		return null;
+	}
+
+	public ArrayList<Location> searchPOIsByQuery(String query) {
+		return null;
+	}
+
+	public ArrayList<Location> searchPOIsByAddress(Address address) {
+		return null;
+	}
+
+	// changed boolean return to void
+	public void addActivePOICategory(int id) {
+		activeCategories.add(id);
+	}
+
+	// changed boolean return to void
+	public void removeActivePOICategory(int id) {
+		if (activeCategories.contains(id)) {
+			activeCategories.remove(id);
+		}
+	}
+}
