08bdb295f86c179acef4a6d5db82b5af2593b936
MStumpp <mstumpp@gmail.com>
2013-06-20 16:01:14 +0200

    added ShortestPathProcessor

--- WalkaRound/server/server-model/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessor.java
+++ WalkaRound/server/server-model/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessor.java
@@ -1,11 +1,68 @@
 package edu.kit.iti.algo2.pse2013.walkaround.server.model;
 
+import edu.kit.iti.algo2.pse2013.walkaround.client.model.route.Route;
+import edu.kit.iti.algo2.pse2013.walkaround.client.model.route.RouteInfo;
+import edu.kit.iti.algo2.pse2013.walkaround.server.graph.Graph;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created with IntelliJ IDEA.
- * User: Matthias
- * Date: 20.06.13
- * Time: 14:45
- * To change this template use File | Settings | File Templates.
+ * ShortestPathProcessor which takes two Coordinates and computes the shortest path between
+ * these coordinates using a Graph model. The result is represented as an ordered list of
+ * Coordinates, whereas the first and last Coordinates equal the input Coordinates.
+ *
+ * The result is passed to a fresh Route object.
+ *
+ * @author Matthias Stumpp
+ * @version 1.0
  */
 public class ShortestPathProcessor {
+
+    private static ShortestPathProcessor instance;
+
+    private Graph graph;
+
+    /**
+     * Creates a fresh instance of ShortestPathProcessor.
+     *
+     * @param graph Graph used for shortest path computation.
+     * @return ShortestPathProcessor.
+     */
+    private ShortestPathProcessor(Graph graph) {
+        this.graph = graph;
+    }
+
+
+    /**
+     * Instantiates and/or returns a singleton instance of ShortestPathProcessor.
+     *
+     * @param graph Graph used for shortest path computation.
+     * @return ShortestPathProcessor.
+     */
+    public static ShortestPathProcessor getInstance(Graph graph) {
+        if (instance == null)
+            instance = new ShortestPathProcessor(graph);
+        return instance;
+    }
+
+
+    /**
+     * Computes a shortest path between any given two Coordinates using the provided
+     * graph.
+     *
+     * @param coordinate1 One end of the route to be computed.
+     * @param coordinate2 Another end of the route to be computed.
+     * @return Route.
+     */
+    public RouteInfo computeShortestPath(Coordinate coordinate1, Coordinate coordinate2) {
+        List<Coordinate> coordinates = new LinkedList<Coordinate>();
+        coordinates.add(new Coordinate(12.12, 12.12));
+        coordinates.add(new Coordinate(13.13, 13.13));
+        coordinates.add(new Coordinate(14.14, 14.14));
+        // TODO: change constructor of route from LinkedList to List, then remove the cast
+        return new Route((LinkedList<Coordinate>) coordinates);
+    }
+
 }
