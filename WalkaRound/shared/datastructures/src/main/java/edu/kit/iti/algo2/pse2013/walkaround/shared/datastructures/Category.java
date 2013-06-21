2ee472854c16fdb05a50448d0e9a6b2a43bb70d3
florian <florian@schaeferban.de>
2013-06-21 17:53:13 +0200

    Implemented Category

--- WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/Category.java
+++ WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/Category.java
@@ -3,4 +3,12 @@ package edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures;
 public class Category {
 
 	private int id;
+
+	public Category(int id) {
+		this.id = id;
+	}
+
+	public int getID() {
+		return id;
+	}
 }
