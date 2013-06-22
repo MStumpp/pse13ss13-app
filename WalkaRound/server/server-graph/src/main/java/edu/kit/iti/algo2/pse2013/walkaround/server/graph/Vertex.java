7370c2f765d1cf3097b0212b2c4f278959cfaf3b
florian <florian@schaeferban.de>
2013-06-22 18:06:17 +0200

    Align server-graph with "Entwurfsdokument"

--- WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
+++ WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
@@ -1,15 +1,18 @@
 package edu.kit.iti.algo2.pse2013.walkaround.server.graph;
 
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
+
 /**
  * This class represents an vertex contained in a graph.
  *
  * @author Matthias Stumpp
  * @version 1.0
  */
-public class Vertex {
+public class Vertex extends Coordinate {
 	private long ID;
-	public Vertex(long l) {
-		this.ID = l;
+	public Vertex(long ID, double lat, double lon) {
+		super(lat, lon);
+		this.ID = ID;
 	}
 	public long getID() {
 		return ID;
