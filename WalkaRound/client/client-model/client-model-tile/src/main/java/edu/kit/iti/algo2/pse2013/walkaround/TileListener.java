8094274be526516c39224186b9ad91927eab03f1
floscher <florian@schaeferban.de>
2013-06-22 15:54:23 +0200

    Added TileFetcher and TileListener

---
+++ WalkaRound/client/client-model/client-model-tile/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/TileListener.java
@@ -0,0 +1,14 @@
+package edu.kit.iti.algo2.pse2013.walkaround;
+
+import java.awt.image.BufferedImage;
+
+public interface TileListener {
+	/**
+	 * ATM the tile comes as BufferedImage. That will be changed in future versions.
+	 * @param tile
+	 * @param x
+	 * @param y
+	 * @param levelOfDetail
+	 */
+	public void receiveTile(final BufferedImage tile, final int x, final int y, final int levelOfDetail);
+}
