598c1b47d062e63cf76331609beea11766d72a3c
florian <florian@schaeferban.de>
2013-06-22 16:31:18 +0200

    Added ID to vertex, comments to OSMDataPreprocessor and switched arguments in GraphDataIO

--- WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
+++ WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
@@ -7,4 +7,11 @@ package edu.kit.iti.algo2.pse2013.walkaround.server.graph;
  * @version 1.0
  */
 public class Vertex {
+	private long ID;
+	public Vertex(long l) {
+		this.ID = l;
+	}
+	public long getID() {
+		return ID;
+	}
 }
