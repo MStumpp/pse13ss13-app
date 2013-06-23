ca7d99cc1476e85f58aae7963de41b688cec5264
Thomas Kadow <tkadow@web.de>
2013-06-23 23:51:37 +0200

    some extensions

--- WalkaRound/client/client-model/client-model-util/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/CoordinateUtility.java
+++ WalkaRound/client/client-model/client-model-util/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/CoordinateUtility.java
@@ -1,8 +1,11 @@
 package edu.kit.iti.algo2.pse2013.walkaround.client.model.util;
 
+import edu.kit.iti.algo2.pse2013.walkaround.client.model.map.DisplayCoordinate;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
 
+import android.util.Log;
 import android.view.Display;
+import android.app.Activity;
 import android.graphics.Point;
 
 public final class CoordinateUtility {
@@ -38,17 +41,22 @@ public final class CoordinateUtility {
 
 	/**
 	 * Calculates the distance between two coordinates
-	 * @param c1 the first coordinate
-	 * @param c2 the the second coordinate
+	 * 
+	 * @param c1
+	 *            the first coordinate
+	 * @param c2
+	 *            the the second coordinate
 	 * @return the distance between the two coordinates in kilometers
 	 * @see {@link https://de.wikipedia.org/wiki/Orthodrome#Strecke}
 	 */
-	public static double calculateDifferenceInMeters(Coordinate c1, Coordinate c2) {
+	public static double calculateDifferenceInMeters(Coordinate c1,
+			Coordinate c2) {
 		double lon1 = Math.toRadians(c1.getLongtitude());
 		double lon2 = Math.toRadians(c2.getLongtitude());
 		double lat1 = Math.toRadians(c1.getLatitude());
 		double lat2 = Math.toRadians(c2.getLatitude());
-		double zeta = Math.acos(Math.sin(lat1)*Math.sin(lat2)+Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1));
-		return zeta*EARTH_RADIUS;
+		double zeta = Math.acos(Math.sin(lat1) * Math.sin(lat2)
+				+ Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
+		return zeta * EARTH_RADIUS;
 	}
 }
--- WalkaRound/client/client-model/client-model-util/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/TileUtility.java
+++ WalkaRound/client/client-model/client-model-util/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/TileUtility.java
@@ -4,24 +4,36 @@ import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
 
 /**
  * @author the team from OpenStreetMap-Wiki
- * @see <a href="http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Java">Slippy Map tilenames</a>
+ * @see <a
+ *      href="http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Java">Slippy
+ *      Map tilenames</a>
  */
 public class TileUtility {
 	/**
-	 * Calculates the x- and y-Part of the tile URL for a given Coordinate anywhere on that tile
-	 * @param c a geographical coordinate which should lie on the tile with the returned indices
-	 * @param levelOfDetail the "tile zoom level" for which the calculation should be made
-	 * @return an array of exactly length 2 containing the x- and y-part of the map-tile URL (in that order)
+	 * Calculates the x- and y-Part of the tile URL for a given Coordinate
+	 * anywhere on that tile
+	 * 
+	 * @param c
+	 *            a geographical coordinate which should lie on the tile with
+	 *            the returned indices
+	 * @param levelOfDetail
+	 *            the "tile zoom level" for which the calculation should be made
+	 * @return an array of exactly length 2 containing the x- and y-part of the
+	 *         map-tile URL (in that order)
 	 */
 	public static int[] getXYTileIndex(Coordinate c, int levelOfDetail) {
 		return new int[] {
-				(int)Math.floor( (c.getLongtitude() + 180) / 360 * (1<<levelOfDetail) ),
-				(int)Math.floor((1 - Math.log(Math.tan(Math.toRadians(c.getLatitude())) + 1 / Math.cos(Math.toRadians(c.getLatitude()))) / Math.PI) / 2 * (1<<levelOfDetail))
-		};
+				(int) Math.floor((c.getLongtitude() + 180) / 360
+						* (1 << levelOfDetail)),
+				(int) Math.floor((1 - Math.log(Math.tan(Math.toRadians(c
+						.getLatitude()))
+						+ 1
+						/ Math.cos(Math.toRadians(c.getLatitude()))) / Math.PI)
+						/ 2 * (1 << levelOfDetail)) };
 	}
 
 	public static Coordinate getUpperLeftCoordinate(int x, int y,
-		int levelOfDetail) {
-	return null;
+			int levelOfDetail) {
+		return null;
 	}
 }
