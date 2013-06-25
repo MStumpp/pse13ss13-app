17dc577a0e48f32e10698d1791b9c295b5b9733a
floscher <florian@schaeferban.de>
2013-06-25 15:33:13 +0200

    Added log-messages to TileFetcher

--- WalkaRound/client/client-model/client-model-tile/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/tile/TileFetcher.java
+++ WalkaRound/client/client-model/client-model-tile/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/client/model/tile/TileFetcher.java
@@ -7,6 +7,7 @@ import java.net.URL;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import edu.kit.iti.algo2.pse2013.walkaround.client.model.map.CurrentMapStyleModel;
 import edu.kit.iti.algo2.pse2013.walkaround.client.model.util.TileUtility;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
@@ -45,10 +46,13 @@ public class TileFetcher {
 	 * @param source
 	 */
 	public boolean requestTiles(final int levelOfDetail, Coordinate c1, Coordinate c2) {
+		Log.d(this.getClass().getSimpleName(), "Check for valid LoD (between max and min).");
 		if (CurrentMapStyleModel.getInstance().getCurrentMapStyle().getMaxLevelOfDetail() < levelOfDetail
 				|| CurrentMapStyleModel.getInstance().getCurrentMapStyle().getMinLevelOfDetail() > levelOfDetail) {
 			return false;
 		}
+		Log.d(this.getClass().getSimpleName(), "LoD valid!");
+		Log.d(this.getClass().getSimpleName(), "Convert GeoCoordinates into Tile-Indices.");
 		final int[] c1XY = TileUtility.getXYTileIndex(c1, levelOfDetail);
 		final int[] c2XY = TileUtility.getXYTileIndex(c2, levelOfDetail);
 
@@ -56,19 +60,25 @@ public class TileFetcher {
 		final int maxX = Math.max(c1XY[0], c2XY[0]);
 		final int minY = Math.min(c1XY[1], c2XY[1]);
 		final int maxY = Math.max(c1XY[1], c2XY[1]);
+		Log.d(this.getClass().getSimpleName(), String.format("x von %s bis %s und y von %s bis %s.", minX, maxX, minY, maxY));
 
-		// TODO: Make asynchronous from here on
-		for (int x = minX; x <= maxX; x++) {
-			for (int y = minY; y <= maxY; y++) {
-				try { // TODO: Find good solution for exception-handling
-					requestTile(x, y, levelOfDetail);
-				} catch (MalformedURLException mue) {
-					mue.printStackTrace();
-				} catch (IOException mue) {
-					mue.printStackTrace();
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int x = minX; x <= maxX; x++) {
+					for (int y = minY; y <= maxY; y++) {
+						Log.d("TileFetcher", String.format("Request tile (%s/%s/%s)"));
+						try {
+							requestTile(x, y, levelOfDetail);
+						} catch (MalformedURLException mue) {
+							Log.d("TileFetcher", "Malformed URL");
+						} catch (IOException mue) {
+							Log.d("TileFetcher", "IOException");
+						}
+					}
 				}
 			}
-		}
+		});
 		return true;
 	}
 
