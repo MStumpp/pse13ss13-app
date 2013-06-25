86c5f7778903e9aa357b5f2a339692d4c23c10a0
florian <florian@schaeferban.de>
2013-06-25 13:07:30 +0200

    Graph changes

--- WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Edge.java
+++ WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Edge.java
@@ -1,10 +1,16 @@
 package edu.kit.iti.algo2.pse2013.walkaround.server.graph;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import edu.kit.iti.algo2.pse2013.walkaround.client.model.util.CoordinateUtility;
 
-public class Edge {
+public class Edge implements Serializable {
+	/**
+	 * Temporary Serial version ID as long as Java serialization is used
+	 */
+	private static final long serialVersionUID = 3182638429343198592L;
+
 	private long id;
 	private ArrayList<Vertex> vertices = new ArrayList<>();
 
--- WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
+++ WalkaRound/server/server-graph/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/graph/Vertex.java
@@ -1,5 +1,7 @@
 package edu.kit.iti.algo2.pse2013.walkaround.server.graph;
 
+import java.io.Serializable;
+
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
 
 /**
@@ -8,7 +10,12 @@ import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
  * @author Matthias Stumpp
  * @version 1.0
  */
-public class Vertex extends Coordinate {
+public class Vertex extends Coordinate implements Serializable {
+	/**
+	 * Temporary Serial version ID as long as Java serialization is used
+	 */
+	private static final long serialVersionUID = -4228194461207025121L;
+
 	private long ID;
 	public Vertex(long ID, double lat, double lon) {
 		super(lat, lon);
