0a2ae929d3f09e61962eedf322d2360dcbeed7ec
MStumpp <mstumpp@gmail.com>
2013-06-27 19:52:06 +0200

    improved performance

--- WalkaRound/server/server-model/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessor.java
+++ WalkaRound/server/server-model/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessor.java
@@ -35,12 +35,36 @@ public class ShortestPathProcessor {
 
 
     /**
+     * Priority queue.
+     */
+    private PriorityQueue<Vertex> queue;
+
+
+    /**
+     * Run counter.
+     */
+    private int runCounter;
+
+
+    /**
      * Creates an instance of ShortestPathProcessor.
      *
      * @param graph Graph used for shortest path computation.
      */
     private ShortestPathProcessor(Graph graph) {
         this.graph = graph;
+        // set up the priority queue
+        queue = new PriorityQueue<>(10, new Comparator<Vertex>() {
+            @Override
+            public int compare(Vertex v1, Vertex v2) {
+                if (v1.getCurrentLength() >  v2.getCurrentLength()){
+                    return 1;
+                } else if (v1.getCurrentLength() < v2.getCurrentLength()){
+                    return -1;
+                } else
+                    return 0;
+            }
+        });
     }
 
 
@@ -75,8 +99,6 @@ public class ShortestPathProcessor {
         //int startVertexId = GeometryProcessor.getInstance(null).getNearestVertex(coordinate1);
         //int endVertexId = GeometryProcessor.getInstance(null).getNearestVertex(coordinate2);
 
-        graph.resetGraph();
-
         int sourceVertexId = 0;
         int targetVertexId = 1000;
 
@@ -92,36 +114,42 @@ public class ShortestPathProcessor {
         if (sourceVertex == null || targetVertex == null)
             return null;
 
-        // set up the queue with the source vertex
-        PriorityQueue<Vertex> queue = new PriorityQueue<>(10, new Comparator<Vertex>() {
-            @Override
-            public int compare(Vertex v1, Vertex v2) {
-                if (v1.getCurrentLength() >  v2.getCurrentLength()){
-                    return 1;
-                } else if (v1.getCurrentLength() < v2.getCurrentLength()){
-                    return -1;
-                } else
-                    return 0;
-            }
-        });
+        // some init
+        runCounter += 1;
+        sourceVertex.setCurrentLength(0.d);
+        queue.clear();
         queue.add(sourceVertex);
 
         // update parent and currentLengths
-        Vertex current;
+        Vertex current, currentHead;
         double distance;
         while (!queue.isEmpty()) {
             current = queue.poll();
-            for (Edge edge : current.getOutgoingEdges()) {
-                distance = current.getCurrentLength() + edge.getLength();
-                if (distance < edge.getHead().getCurrentLength()) {
-                    edge.getHead().setCurrentLength(distance);
-                    edge.getHead().setParent(current);
-                    if (!queue.contains(edge.getHead()))
-                        queue.add(edge.getHead());
-                }
-            }
+            // stop criteria
             if (current.equals(targetVertex))
                 break;
+            for (Edge edge : current.getOutgoingEdges()) {
+                currentHead = edge.getHead();
+                distance = current.getCurrentLength() + edge.getLength();
+
+                // not yet visited during current run
+                if (currentHead.getRun() != runCounter) {
+                    currentHead.setCurrentLength(distance);
+                    currentHead.setParent(current);
+                    queue.add(currentHead);
+                    currentHead.setRun(runCounter);
+
+                // edhe head already in queue, eventually decrease key
+                } else if (distance < currentHead.getCurrentLength()) {
+                    currentHead.setCurrentLength(distance);
+                    currentHead.setParent(current);
+                    // remove head if its already in the queue, coz
+                    // updating current length after added to queue doesn't
+                    // lead to order change
+                    queue.remove(currentHead);
+                    queue.add(currentHead);
+                }
+            }
         }
 
         // get the list of coordinates
