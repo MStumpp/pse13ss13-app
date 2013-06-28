8dcb5dcd1083da6d252164a22dfa8f3da9357cd9
MStumpp <mstumpp@gmail.com>
2013-06-28 14:13:59 +0200

    some fixes

--- WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/CoordinateTest.java
+++ WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/datastructures/CoordinateTest.java
@@ -3,12 +3,10 @@ package edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class CoordinateTest {
 	@Test
-    @Ignore
     public void testValidLonValues() {
 		new Coordinate(0, -180);
 		new Coordinate(0, Math.E*-50);
@@ -19,7 +17,6 @@ public class CoordinateTest {
 		new Coordinate(0, 180);
 	}
 	@Test
-    @Ignore
     public void testValidLatValues() {
 		new Coordinate(-45, 0);
 		new Coordinate(Math.E*-25, 0);
@@ -29,29 +26,24 @@ public class CoordinateTest {
 		new Coordinate(45, 0);
 	}
 	@Test
-    @Ignore
 	public void testLonOverflow() {
 		new Coordinate(0, 180.0000000000001);
 	}
 	@Test
-    @Ignore
 	public void testLonUnderflow() {
 		new Coordinate(0, -180.0000000000001);
 	}
 	@Test
-    @Ignore
     public void testLatOverflow() {
 		assertEquals(new Coordinate(90.00000000000001, 0).getLatitude(), -89.99999999999999, 1e-13);
 	}
 	@Test
-    @Ignore
     public void testLatUnderflow() {
 		System.err.println(-1 % 180);
 		System.err.println(-181 % 180);
 		assertEquals(new Coordinate(-90.00000000000001, 0).getLatitude(), 89.99999999999999, 1);
 	}
 	@Test
-    @Ignore
     public void testLonPersistence() {
 		for (double lon = -180; lon <= 180; lon+= 30) {
 			for (double lat = -90; lat <= 90; lat += 90) {
@@ -60,7 +52,6 @@ public class CoordinateTest {
 		}
 	}
 	@Test
-    @Ignore
 	public void testLatPersistence() {
 		for (double lon = -180; lon <= 180; lon+= 90) {
 			for (double lat = -90; lat <= 90; lat += 30) {
