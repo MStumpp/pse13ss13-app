f55b54cda27c5224b15f3102542cd3a9d09a8852
MStumpp <mstumpp@gmail.com>
2013-06-29 18:08:09 +0200

    some tests

--- WalkaRound/server/server-model/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessorTest.java
+++ WalkaRound/server/server-model/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/server/model/ShortestPathProcessorTest.java
@@ -19,68 +19,6 @@ import org.junit.Test;
 public class ShortestPathProcessorTest {
 
     @Test
-    @Ignore
-    public void testPreprocessGraphDataIO() {
-
-        File verticesFile = new File("/Users/Matthias/Workspace/PSE/data/_nodes.txt");
-        FileReader input;
-        try {
-            input = new FileReader(verticesFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Map<Long, Vertex> vertices = new HashMap<>();
-
-        BufferedReader bufRead = new BufferedReader(input);
-        String myLine;
-        Vertex current;
-        long osmID;
-        try {
-            while ((myLine = bufRead.readLine()) != null)
-            {
-                String[] array = myLine.split("\\s");
-                osmID = Long.parseLong(array[0]);
-                current = new Vertex(Double.parseDouble(array[1]), Double.parseDouble(array[2]));
-                vertices.put(osmID, current);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        File edgesFile = new File("/Users/Matthias/Workspace/PSE/data/_edges.txt");
-        try {
-            input = new FileReader(edgesFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        GraphDataIO graphDataIO = new GraphDataIO();
-
-        bufRead = new BufferedReader(input);
-        try {
-            while ((myLine = bufRead.readLine()) != null)
-            {
-                String[] array = myLine.split("\\s");
-                graphDataIO.addEdge(new Edge(vertices.get(Long.parseLong(array[1])), vertices.get(Long.parseLong(array[2]))));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        try {
-            GraphDataIO.save(graphDataIO, new File("/Users/Matthias/Workspace/PSE/data/graphDataIO"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Test
     public void testLoadGraphDataIO() {
 
         GraphDataIO graphDataIO = null;
