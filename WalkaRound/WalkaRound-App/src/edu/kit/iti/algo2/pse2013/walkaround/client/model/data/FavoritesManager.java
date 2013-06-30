93eb84ecf721a2b77b49065a4e8ba4bd3d08063f
Thomas Kadow <tkadow@web.de>
2013-06-30 23:52:10 +0200

    extended managers

--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/FavoritesManager.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/FavoritesManager.java
@@ -5,19 +5,42 @@ import java.util.ArrayList;
 import edu.kit.iti.algo2.pse2013.walkaround.client.model.route.RouteInfo;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Location;
 
+/**
+ * This class manages all saved locations and routes.
+ * 
+ * @author Thomas Kadow
+ * @version 1.0
+ */
 public class FavoritesManager {
 
+	/**
+	 * Instance of the FavoritesManager.
+	 */
 	private static FavoritesManager instance;
 
+	/**
+	 * List of all saved routes.
+	 */
 	private ArrayList<RouteInfo> savedRoutes;
 
+	/**
+	 * List of all saved locations.
+	 */
 	private ArrayList<Location> savedLocations;
 
+	/**
+	 * Constructs a new manager for the favorites.
+	 */
 	private FavoritesManager() {
 		savedRoutes = new ArrayList<RouteInfo>();
 		savedLocations = new ArrayList<Location>();
 	}
 
+	/**
+	 * Singleton getInstance method.
+	 * 
+	 * @return an instance of the FavoritesManager
+	 */
 	public static FavoritesManager getInstance() {
 		if (instance == null) {
 			instance = new FavoritesManager();
@@ -25,51 +48,119 @@ public class FavoritesManager {
 		return instance;
 	}
 
+	/**
+	 * Returns a list of all saved routes.
+	 * 
+	 * @return a list of all saved routes
+	 */
 	public ArrayList<RouteInfo> getFavoriteRoutes() {
 		return savedRoutes;
 	}
 
+	/**
+	 * Returns a list of all saved locations.
+	 * 
+	 * @return a list of all saved locations
+	 */
 	public ArrayList<Location> getFavoriteLocations() {
 		return savedLocations;
 	}
 
+	/**
+	 * Returns a favorite route by ID.
+	 * 
+	 * @param index
+	 *            index of the favorite route.
+	 * @return RouteInfo
+	 */
 	public RouteInfo getFavoriteRoute(int index) {
 		return savedRoutes.get(index);
 	}
 
+	/**
+	 * Returns a favorite location by ID.
+	 * 
+	 * @param index
+	 *            index of the favorite location.
+	 * @return Location
+	 */
 	public Location getFavoriteLocation(int index) {
 		return savedLocations.get(index);
 	}
 
 	// changed boolean return to void
+	/**
+	 * Removes a favorite route from the list.
+	 * 
+	 * @param index
+	 *            index of the favorite route
+	 */
 	public void deleteRoute(int index) {
 		savedRoutes.remove(index);
 	}
 
 	// changed boolean return to void
+	/**
+	 * Removes a favorite location from the list.
+	 * 
+	 * @param index
+	 *            index of the favorite location
+	 */
 	public void deleteLocation(int index) {
 		savedLocations.remove(index);
 	}
 
 	// changed boolean return to void
+	/**
+	 * Adds a route to the list of favorite routes.
+	 * 
+	 * @param routeToSave
+	 *            RouteInfo to be saved
+	 * @param name
+	 *            name for the favorite
+	 */
 	public void addRouteToFavorites(RouteInfo routeToSave, String name) {
-		if (!routeToSave.isFavorite()) {
+		if (!savedRoutes.contains(routeToSave)) {
 			savedRoutes.add(routeToSave);
 		}
 	}
 
 	// changed boolean return to void
+	/**
+	 * Adds a location to the list of favorite locations.
+	 * 
+	 * @param locationToSave
+	 *            Location to be saved
+	 * @param name
+	 *            name for the favorite
+	 */
 	public void addLocationToFavorites(Location locationToSave, String name) {
-		if (!locationToSave.isFavorite()) {
+		if (!savedLocations.contains(locationToSave)) {
 			savedLocations.add(locationToSave);
 		}
 	}
 
+	/**
+	 * Returns whether the given RouteInfo is a favorite.
+	 * 
+	 * @param routeInfo
+	 *            RouteInfo to check
+	 * @return true if it is a favorite, false otherwise
+	 */
 	public boolean containsRoute(RouteInfo routeInfo) {
 		return savedRoutes.contains(routeInfo);
 	}
 
+	/**
+	 * Returns whether the given Location is a favorite.
+	 * 
+	 * @param location
+	 *            Location to check
+	 * @return true if it is a favorite, false otherwise
+	 */
 	public boolean containsLocation(Location location) {
 		return savedLocations.contains(location);
 	}
+	
+	//Nach schließen der app müssen favoriten gespeichert bleiben
 }
--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/POIManager.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/data/POIManager.java
@@ -1,6 +1,7 @@
 package edu.kit.iti.algo2.pse2013.walkaround.client.model.data;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import edu.kit.iti.algo2.pse2013.walkaround.client.model.route.RouteInfo;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Address;
@@ -9,19 +10,47 @@ import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Location;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.LocationDataIO;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.POI;
 
+/**
+ * This class manages requests about POIs.
+ * 
+ * @author Thomas Kadow
+ * @version 1.0
+ */
 public class POIManager {
 
+	/**
+	 * Instance of the POIManager.
+	 */
 	private static POIManager instance;
 
+	/**
+	 * LocationDataIO where all POIs are stored.
+	 */
 	private LocationDataIO locationDataIO;
 
+	/**
+	 * IDs of categories that where activated in the POI menu.
+	 */
 	private int[] activeCategories;
 
+	/**
+	 * Constructs a new manager for POIs.
+	 * 
+	 * @param locationDataIO
+	 *            LocationDataIO object
+	 */
 	private POIManager(LocationDataIO locationDataIO) {
 		this.locationDataIO = locationDataIO;
 		activeCategories = new int[20];
 	}
 
+	/**
+	 * Singleton getInstance method.
+	 * 
+	 * @param locationDataIO
+	 *            LocationDataIO object
+	 * @return an instance of the POIManager
+	 */
 	public static POIManager getInstance(LocationDataIO locationDataIO) {
 		if (instance == null) {
 			instance = new POIManager(locationDataIO);
@@ -29,45 +58,123 @@ public class POIManager {
 		return instance;
 	}
 
+	/**
+	 * Returns the IDs of all active categories.
+	 * 
+	 * @return int[] of IDs of all active categories
+	 */
 	public int[] getActiveCategories() {
 		return activeCategories;
 	}
 
 	// int[] parameter gelöscht da aktive kategorien als attribut vorliegen
+	/**
+	 * Returns all POIs laying within a rectangle.
+	 * 
+	 * @param upperLeft
+	 *            upperleft coordinate of the rectangle
+	 * @param bottomRight
+	 *            bottomright coordinate of the rectangle
+	 * @param levelOfDetail
+	 *            level of detail
+	 * @return a list of all POIs laying within a rectangle
+	 */
 	public ArrayList<POI> getPOIsWithinRectangle(Coordinate upperLeft,
 			Coordinate bottomRight, int levelOfDetail) {
 		return null;
 	}
 
 	// int[] parameter gelöscht da aktive kategorien als attribut vorliegen
+	/**
+	 * Returns all POIs laying upon a route.
+	 * 
+	 * @param routeInfo
+	 *            Route to search POIs in the near
+	 * @param levelOfDetail
+	 *            level of detail
+	 * @return a list of all POIs laying upon a route
+	 */
 	public ArrayList<POI> getPOIsAlongRoute(RouteInfo routeInfo,
 			int levelOfDetail) {
 		return null;
 	}
 
-	public ArrayList<Location> searchPOIsByQuery(String query) {
+	// aus list Locations[] gemacht
+	/**
+	 * Returns three suggestions of locations searched by query.
+	 * 
+	 * @param query
+	 *            query to search with
+	 * @return a list of three suggestions of locations
+	 */
+	public Location[] searchPOIsByQuery(String query) {
+		Location[] suggestions = new Location[3];
+		for (Iterator<POI> iter = locationDataIO.getPOIs().iterator(); iter
+				.hasNext();) {
+			POI current = iter.next();
+			int similarity = computeLevenstheinDistance(query,
+					current.getName());
+
+		}
 		return null;
 	}
 
-	public ArrayList<Location> searchPOIsByAddress(Address address) {
+	// aus list Locations[] gemacht
+	/**
+	 * Returns three suggestions of locations searched by an address.
+	 * 
+	 * @param address
+	 *            address to search with
+	 * @return a list of three suggestions of locations
+	 */
+	public Location[] searchPOIsByAddress(Address address) {
+		Location[] suggestions = new Location[3];
+		for (Iterator<POI> iter = locationDataIO.getPOIs().iterator(); iter
+				.hasNext();) {
+			POI current = iter.next();
+			/*int similarity = computeLevenstheinDistance(,
+					current.getName());*/
+
+		}
 		return null;
 	}
 
 	// changed boolean return to void
+	/**
+	 * Adds the ID of an active category.
+	 * 
+	 * @param id
+	 *            id of the category to activate
+	 */
 	public void addActivePOICategory(int id) {
-		for (int i = 0; i < activeCategories.length; i++) {
-			if(activeCategories[i] )
+		if (activeCategories[id] == 0) {
+			activeCategories[id] = id;
 		}
 	}
 
 	// changed boolean return to void
+	/**
+	 * Removes the ID of an active category.
+	 * 
+	 * @param id
+	 *            id of the category to deactivate
+	 */
 	public void removeActivePOICategory(int id) {
-		if (activeCategories.contains(id)) {
-			activeCategories.remove(id);
+		if (activeCategories[id] == id) {
+			activeCategories[id] = 0;
 		}
 	}
 
-	public static int computeLevenstheinDistance(String first, String second) {
+	/**
+	 * Computes the difference between two strings.
+	 * 
+	 * @param first
+	 *            first string to compare
+	 * @param second
+	 *            second string to compare
+	 * @return an int of the difference between to strings
+	 */
+	private int computeLevenstheinDistance(String first, String second) {
 		int matrix[][] = new int[first.length() + 1][second.length() + 1];
 		for (int i = 0; i < first.length() + 1; i++) {
 			matrix[i][0] = i;
