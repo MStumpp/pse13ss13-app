62bcd9393791a4e92785edf379021789ab755223
MStumpp <mstumpp@gmail.com>
2013-07-08 19:24:54 +0200

    major fixes

--- WalkaRound/preprocessor/preprocessor-model/preprocessor-model-geometry/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/preprocessor/model/geometry/GeometryNode.java
+++ WalkaRound/preprocessor/preprocessor-model/preprocessor-model-geometry/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/preprocessor/model/geometry/GeometryNode.java
@@ -19,11 +19,28 @@ public class GeometryNode implements Serializable {
 
     private double splitValue;
 
+    private GeometryNode parent;
+
+    private int depth;
+
     private GeometryNode leftNode;
 
     private GeometryNode rightNode;
 
     private Vertex vertex;
+
+    public GeometryNode(GeometryNode parent, int depth, Vertex vertex) {
+        this.parent = parent;
+        this.depth = depth;
+        this.vertex = vertex;
+        this.splitValue = Double.NaN;
+    }
+
+    public GeometryNode(GeometryNode parent, int depth, double splitValue) {
+        this.parent = parent;
+        this.depth = depth;
+        this.splitValue = splitValue;
+    }
 
     public GeometryNode(Vertex vertex) {
         this.vertex = vertex;
@@ -35,6 +52,14 @@ public class GeometryNode implements Serializable {
 
     public double getSplitValue() {
         return splitValue;
+    }
+
+    public GeometryNode getParent() {
+        return parent;
+    }
+
+    public int getDepth() {
+        return depth;
     }
 
     public GeometryNode getLeftNode() {
@@ -61,20 +86,34 @@ public class GeometryNode implements Serializable {
         this.vertex = vertex;
     }
 
+    public boolean isRoot() {
+        return parent == null;
+    }
+
+    public boolean isLeaf() {
+        return vertex != null;
+    }
+
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
-        //sb.append("\n" + vertex.toString() + "\n");
-        if (leftNode != null)
-        //    sb.append("-> leftNode: " + leftNode.getVertex().toString() + "\n");
-        if (rightNode != null)
-        //    sb.append("-> rightNode: " + rightNode.getVertex().toString() + "\n");
+        sb.append("\n\n -> depth: " + depth + "\n");
+        sb.append("-> split: " + splitValue + "\n");
+
+        if (vertex != null)
+            sb.append("\n vertex: " + vertex.toString() + "\n");
+        else
+            sb.append("\n no vertex \n");
+
+        if (parent != null)
+            sb.append("-> parent: id: \n");
+        else
+            sb.append("-> no parent \n");
 
         if (leftNode != null)
-            sb.append(leftNode.toString() + "\n");
+            sb.append("-> leftNode: " + leftNode.toString() + "\n");
         if (rightNode != null)
-            sb.append(rightNode.toString() + "\n");
-
+            sb.append("-> rightNode: " + rightNode.toString() + "\n");
         return sb.toString();
     }
 
