62b8c093ea80f528e5d21355f7e3bcbef58dd49a
floscher <florian@schaeferban.de>
2013-07-09 20:58:56 +0200

    Updated Protobuf Input/Output.

--- WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/pbf/ProtobufConverter.java
+++ WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/pbf/ProtobufConverter.java
@@ -1,12 +1,6 @@
 package edu.kit.iti.algo2.pse2013.walkaround.shared.pbf;
 
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-
-import com.google.protobuf.Message;
 
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Address;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
@@ -20,39 +14,8 @@ import edu.kit.iti.algo2.pse2013.walkaround.shared.pbf.LocationProtos.SaveLocati
 import edu.kit.iti.algo2.pse2013.walkaround.shared.pbf.LocationProtos.SaveLocationData.SaveLocation.SaveAddress;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.pbf.LocationProtos.SaveLocationData.SavePOI;
 
-public class ProtobufIO {
-	public static Coordinate readCoordinate(InputStream fileStream) throws IOException {
-		return coordinateFromBuilder(SaveCoordinate.parseFrom(fileStream));
-	}
-	public static Location readLocation(InputStream fileStream) throws IOException {
-		return locationFromBuilder(SaveLocation.parseFrom(fileStream));
-	}
-	public static LocationDataIO readLocationData(InputStream fileStream) throws IOException {
-		return locationDataFromBuilder(SaveLocationData.parseFrom(fileStream));
-	}
-	public static POI readPOI(InputStream fileStream) throws IOException {
-		return poiFromBuilder(SavePOI.parseFrom(fileStream));
-	}
-	public static void write(Coordinate c, OutputStream fileStream) throws IOException {
-		write(coordinateToBuilder(c), fileStream);
-	}
-	public static void write(Location l, OutputStream fileStream) throws IOException {
-		write(locationToBuilder(l), fileStream);
-	}
-	public static void write(LocationDataIO l, OutputStream fileStream) throws IOException {
-		write(locationDataToBuilder(l), fileStream);
-	}
-	public static void write(POI p, OutputStream fileStream) throws IOException {
-		write(poiToBuilder(p), fileStream);
-	}
-
-	private static void write(Message.Builder builder, OutputStream fileStream) throws IOException {
-		builder.build().writeTo(fileStream);
-		fileStream.flush();
-		fileStream.close();
-	}
-
-	private static Address addressFromBuilder(SaveAddress saveAddress) {
+public class ProtobufConverter {
+	public static Address getAddress(SaveAddress saveAddress) {
 		if (saveAddress == null) {
 			return null;
 		}
@@ -62,7 +25,7 @@ public class ProtobufIO {
 			saveAddress.getCity(),
 			saveAddress.getPostalCode());
 	}
-	private static SaveAddress.Builder addressToBuilder(Address a) {
+	public static SaveAddress.Builder getAddressBuilder(Address a) {
 		if (a == null) {
 			return null;
 		}
@@ -72,7 +35,7 @@ public class ProtobufIO {
 				.setPostalCode(a.getPostalCode())
 				.setStreet(a.getStreet());
 	}
-	private static Coordinate coordinateFromBuilder(SaveCoordinate saveCoord) {
+	public static Coordinate getCoordinate(SaveCoordinate saveCoord) {
 		float[] crossroads = new float[saveCoord.getCrossroadAngleCount()];
 		for (int i = 0; i < saveCoord.getCrossroadAngleCount(); i++) {
 			crossroads[i] = saveCoord.getCrossroadAngle(i);
@@ -83,64 +46,66 @@ public class ProtobufIO {
 		}
 		return new Coordinate(saveCoord.getLatitude(), saveCoord.getLongitude(), crossInfo);
 	}
-	private static SaveCoordinate.Builder coordinateToBuilder(Coordinate coord) {
+	public static SaveCoordinate.Builder getCoordinateBuilder(Coordinate c) {
 		SaveCoordinate.Builder saveCoord = SaveCoordinate.newBuilder()
-				.setLatitude(coord.getLatitude())
-				.setLongitude(coord.getLongitude());
-		if (coord.getCrossingInformation() != null && coord.getCrossingInformation().getCrossingAngles() != null) {
-			for (float angle : coord.getCrossingInformation().getCrossingAngles()) {
+				.setLatitude(c.getLatitude())
+				.setLongitude(c.getLongitude());
+		if (c.getCrossingInformation() != null && c.getCrossingInformation().getCrossingAngles() != null) {
+			for (float angle : c.getCrossingInformation().getCrossingAngles()) {
 				saveCoord.addCrossroadAngle(angle);
 			}
 		}
 		return saveCoord;
 	}
-	private static Location locationFromBuilder(SaveLocation saveLoc) {
+
+	public static Location getLocation(SaveLocation saveLoc) {
 		return new Location(
 			saveLoc.getParent().getLatitude(),
 			saveLoc.getParent().getLongitude(),
 			saveLoc.getID(),
 			saveLoc.getName(),
-			addressFromBuilder(saveLoc.getAddress()));
+			getAddress(saveLoc.getAddress()));
 	}
-	private static SaveLocation.Builder locationToBuilder(Location loc) {
+	public static SaveLocation.Builder getLocationBuilder(Location loc) {
 		SaveLocation.Builder builder = SaveLocation.newBuilder()
-				.setParent(coordinateToBuilder(loc))
+				.setParent(getCoordinateBuilder(loc))
 				.setID(loc.getId())
 				.setName(loc.getName());
 		if (loc.getAddress() != null) {
-			builder.setAddress(addressToBuilder(loc.getAddress()));
+			builder.setAddress(getAddressBuilder(loc.getAddress()));
 		}
 		return builder;
 	}
-	private static LocationDataIO locationDataFromBuilder(SaveLocationData saveLocationData) {
+
+	public static LocationDataIO getLocationData(SaveLocationData saveLocationData) {
 		LocationDataIO locationData = new LocationDataIO();
 		for (SavePOI savePOI : saveLocationData.getPOIList()) {
-			locationData.addPOI(poiFromBuilder(savePOI));
+			locationData.addPOI(getPOI(savePOI));
 		}
 		return locationData;
 	}
-	private static SaveLocationData.Builder locationDataToBuilder(LocationDataIO l) {
+	public static SaveLocationData.Builder getLocationDataBuilder(LocationDataIO l) {
 		SaveLocationData.Builder builder = SaveLocationData.newBuilder();
 		for (POI p : l.getPOIs()) {
-			builder.addPOI(poiToBuilder(p));
+			builder.addPOI(getPOIBuilder(p));
 		}
 		return builder;
 	}
-	private static POI poiFromBuilder(SavePOI savePOI) {
+	public static POI getPOI(SavePOI savePOI) {
 		int[] cats = new int[savePOI.getPOICategoryList().size()];
 		for (int i = 0; i < cats.length; i++) {
 			cats[i] = savePOI.getPOICategory(i);
 		}
-		return new POI(locationFromBuilder(savePOI.getParent()), savePOI.getTextInfo(), savePOI.getImageURL(), cats);
+		return new POI(getLocation(savePOI.getParent()), savePOI.getTextInfo(), savePOI.getImageURL(), cats);
 	}
-	private static SavePOI.Builder poiToBuilder(POI p) {
+	public static SavePOI.Builder getPOIBuilder(POI p) {
 		int[] cats = p.getPOICategories();
 		ArrayList<Integer> poiList = new ArrayList<Integer>();
 		for (int i = 0; i < cats.length; i++) {
 			poiList.add(cats[i]);
 		}
 		return SavePOI.newBuilder()
-				.setParent(locationToBuilder(p))
+				.setParent(getLocationBuilder(p))
 				.setTextInfo(p.getTextInfo())
 				.setImageURL(p.getURL())
 				.addAllPOICategory(poiList);
