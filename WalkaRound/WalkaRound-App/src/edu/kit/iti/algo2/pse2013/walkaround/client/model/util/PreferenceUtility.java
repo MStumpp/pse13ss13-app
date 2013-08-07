cdb706069eb64300f6b380bdd82bb1e20a4e3f27
Ludwig <ludi.biermann@gmail.com>
2013-08-07 14:08:25 +0200

    fix github errors

--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/PreferenceUtility.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/util/PreferenceUtility.java
@@ -54,7 +54,6 @@ public class PreferenceUtility {
 	}
 	
 	public String getMapStyle(){
-<<<<<<< HEAD
 		return sh.getString(context.getResources().getString(R.string.options_map_typ), "Mapnik");
 	}
 	
@@ -64,9 +63,6 @@ public class PreferenceUtility {
 
 	public boolean isPOITextSoundOn(){
 		return  sh.getBoolean(context.getString(R.string.options_poi_text_output), true);
-=======
-		return PreferenceManager.getDefaultSharedPreferences(context).getString(context.getResources().getString(R.string.options_map_typ), "Mapnik");
->>>>>>> 1f9bd8f0aa779c45e1e6cb8c29acc85b3f057a70
 	}
 	
 	public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener){
