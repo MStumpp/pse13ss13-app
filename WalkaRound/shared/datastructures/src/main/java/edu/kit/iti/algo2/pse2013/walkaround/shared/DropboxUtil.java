f84a9227bab5d156dbc1a69774368a7e1b7c80f2
floscher <florian@schaeferban.de>
2013-08-09 15:29:15 +0200

    Fixed LocationData-URL-Bug

--- WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/DropboxUtil.java
+++ WalkaRound/shared/datastructures/src/main/java/edu/kit/iti/algo2/pse2013/walkaround/shared/DropboxUtil.java
@@ -4,9 +4,9 @@ import java.io.File;
 
 public class DropboxUtil {
 	public static String getDropbox() {
-		if (System.getProperty("user.name").equals("Lukas")) {
+		if ("Lukas".equals(System.getProperty("user.name"))) {
 			return System.getProperty("user.home") + File.separatorChar + "Desktop" + File.separatorChar + "Dropbox" + File.separatorChar + "Studium" + File.separatorChar + "PSE";
 		}
-		return System.getProperty("user.home") + File.separatorChar + "Dropbox" + File.separatorChar + "Studium" + File.separatorChar + "PSE";
+		return System.getProperty("user.home") + File.separatorChar + "Dropbox" + File.separatorChar + "Studium" + File.separatorChar + "PSE" + File.separatorChar;
 	}
 }
