9f45ea0bf2f56e31f92a8335e0f9f77369959a26
floscher <florian@schaeferban.de>
2013-08-09 13:57:20 +0200

    Fixed POIImageFetcher (expects an URL now)

--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/test/client/model/util/POIImageFetcherTest.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/test/client/model/util/POIImageFetcherTest.java
@@ -1,22 +1,22 @@
 package edu.kit.iti.algo2.pse2013.walkaround.test.client.model.util;
 
-import edu.kit.iti.algo2.pse2013.walkaround.client.model.util.CoordinateUtility;
-import edu.kit.iti.algo2.pse2013.walkaround.client.model.util.POIImageFetcher;
-import junit.framework.Assert;
-import android.test.AndroidTestCase;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import java.io.IOException;
+import org.junit.Test;
 
 import android.graphics.Bitmap;
 import android.test.AndroidTestCase;
+import edu.kit.iti.algo2.pse2013.walkaround.client.model.util.POIImageFetcher;
 
 
 public class POIImageFetcherTest extends AndroidTestCase {
 
-	public void testImageFetcher() {
+	@Test
+	public void testImageFetcher() throws MalformedURLException {
 		Bitmap bitmap;
 		bitmap = POIImageFetcher
-				.fetchImage("http://commons.wikimedia.org/wiki/File:Gray_vacuum_cleaner.svg");
+				.fetchImage(new URL("http://commons.wikimedia.org/wiki/File:Gray_vacuum_cleaner.svg"));
 		assertTrue(bitmap != null);
 
 	}
