d782a1cfa6be9a040bb561f82831c50498d494ad
MStumpp <mstumpp@gmail.com>
2013-08-16 21:52:30 +0200

    ...

--- WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/geometry/GeometrizablePOIConstraintTest.java
+++ WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/geometry/GeometrizablePOIConstraintTest.java
@@ -1,15 +1,12 @@
 package edu.kit.iti.algo2.pse2013.walkaround.shared.geometry;
 
-import edu.kit.iti.algo2.pse2013.walkaround.shared.graph.Vertex;
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.POI;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.Field;
-import java.util.LinkedList;
-
 /**
- * GeometryDataIOTest.
+ * GeometrizablePOIConstraintTest
  *
  * @author Matthias Stumpp
  * @version 1.0
@@ -21,10 +18,21 @@ public class GeometrizablePOIConstraintTest {
 
     }
 
+    @Test
+    public void testInitialization() {
+        new GeometrizablePOIConstraint(new int[] {1, 2});
+    }
 
     @Test
-    public void testInitialization1() {
+    public void testIsValidMethod() {
+        GeometrizablePOIConstraint instance = new GeometrizablePOIConstraint(new int[] {1, 2, 3, 4, 5});
+        Assert.assertTrue(instance.isValid(new POI(1.d, 2.d, "name", "info", null, new int[] { 1 }, null)));
+    }
 
+    @Test
+    public void testIsNotValidMethod() {
+        GeometrizablePOIConstraint instance = new GeometrizablePOIConstraint(new int[] {1, 2, 3, 4, 5});
+        Assert.assertTrue(instance.isValid(new POI(1.d, 2.d, "name", "info", null, new int[] { 6 }, null)));
     }
 
 }
---
+++ WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/geometry/GeometrizableWrapperTest.java
@@ -0,0 +1,77 @@
+package edu.kit.iti.algo2.pse2013.walkaround.shared.geometry;
+
+import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.POI;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * GeometrizableWrapperTest
+ *
+ * @author Matthias Stumpp
+ * @version 1.0
+ */
+public class GeometrizableWrapperTest {
+
+    @Test
+    public void testInit() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+    }
+
+    @Test
+    public void testNumberDimensions() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertEquals(2, instance.numberDimensions());
+    }
+
+    @Test
+    public void testValueForDimension1() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertEquals(1.d, instance.valueForDimension(0), 0.d);
+    }
+
+    @Test
+    public void testValueForDimension2() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertEquals(2.d, instance.valueForDimension(1), 0.d);
+    }
+
+    @Test
+    public void testNumberNodes() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertEquals(1, instance.numberNodes());
+    }
+
+    @Test
+    public void testNodeNotNull1() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertNotNull(instance.getNode(0));
+    }
+
+    @Test
+    public void testNodeNotNull2() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertNotNull(instance.getNode());
+    }
+
+    @Test
+    public void testNodeNotNullGeometrizable() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertNotNull(instance.getGeometrizable());
+    }
+
+    @Test
+    public void testToString() {
+        POI poi = new POI(1.d, 2.d, "name", "info", null, new int[]{1}, null);
+        GeometrizableWrapper instance = new GeometrizableWrapper(poi, 1);
+        Assert.assertNotNull(instance.toString());
+    }
+
+}
