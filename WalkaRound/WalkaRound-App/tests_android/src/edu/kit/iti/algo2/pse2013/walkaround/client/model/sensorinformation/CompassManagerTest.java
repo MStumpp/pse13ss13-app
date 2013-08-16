3079e953395eeb47d934ae77e913bb718ff779f8
Ludwig <ludi.biermann@gmail.com>
2013-08-16 19:37:18 +0200

    add Tests

--- WalkaRound/WalkaRound-App/tests_android/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/sensorinformation/CompassManagerTest.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.iti.algo2.pse2013.walkaround.client.model.sensorinformation;
-
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-@RunWith(RobolectricTestRunner.class)
-public class CompassManagerTest {
-
-}
