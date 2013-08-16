4fe3dcb30240526b29b553f7b1dc0a079602d6e7
MStumpp <mstumpp@gmail.com>
2013-08-16 21:16:42 +0200

    ...

---
+++ WalkaRound/shared/datastructures/src/test/java/edu/kit/iti/algo2/pse2013/walkaround/shared/geometry/GeometrizablePOIConstraintTest.java
@@ -0,0 +1,30 @@
+package edu.kit.iti.algo2.pse2013.walkaround.shared.geometry;
+
+import edu.kit.iti.algo2.pse2013.walkaround.shared.graph.Vertex;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+
+/**
+ * GeometryDataIOTest.
+ *
+ * @author Matthias Stumpp
+ * @version 1.0
+ */
+public class GeometrizablePOIConstraintTest {
+
+    @Before
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+
+    }
+
+
+    @Test
+    public void testInitialization1() {
+
+    }
+
+}
