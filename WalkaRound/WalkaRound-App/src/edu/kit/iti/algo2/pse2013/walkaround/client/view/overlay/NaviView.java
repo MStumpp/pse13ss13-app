f7899686549d42ce7382ec58b35aacb5a607b12a
Lukas Müller <lukasmuller@gmx.net>
2013-09-09 22:06:53 +0200

    added draft for NaviView

---
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/view/overlay/NaviView.java
@@ -0,0 +1,52 @@
+package edu.kit.iti.algo2.pse2013.walkaround.client.view.overlay;
+
+import java.util.LinkedList;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import edu.kit.iti.algo2.pse2013.walkaround.client.R;
+
+/**
+ * This class shows the navigation elements such as:
+ * - a textfield
+ * - a pictogramm
+ * - the Play/Pause Button to activate/deactivate the navi
+ * - various information tabs such as speed, time on route, etc.
+ * 
+ * @author Lukas Müller
+ * @version 0.1
+ * 
+ */
+public class NaviView extends RelativeLayout {
+	
+	private final static String TAG = NaviView.class.getSimpleName();
+	
+	public NaviView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+		
+	// TODO:
+	// Play/Pause Button
+	// Textfeld
+	// Piktogramme
+	// Verbindung mit Modell-Funktionen aus NaviModel
+	// Verbindung mit Optionen
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
