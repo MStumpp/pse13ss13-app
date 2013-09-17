0937900ef313ab3b35f1f6ccc45035e52bf6ac90
Ludwig Biermann <ludi.biermann@gmail.com>
2013-09-17 14:38:27 +0200

    fix bounding finger zooming

--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/controller/activity/WalkaRound.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/controller/activity/WalkaRound.java
@@ -307,7 +307,7 @@ public class WalkaRound extends Activity implements HeadUpViewListener,
 
 		// shifting to new Point
 		private final static float SHIFT_PER_PX = 0.08F;
-		
+
 		// Distance between Two Fingers
 		private float distance = 1f;
 
@@ -328,11 +328,11 @@ public class WalkaRound extends Activity implements HeadUpViewListener,
 			case MotionEvent.ACTION_DOWN:
 				break;
 			case MotionEvent.ACTION_POINTER_DOWN:
-				distance = spacing(event);
+				distance = computeDistanceBetweenFingers(event);
 				Log.d(TAG, "oldDist=" + distance);
 				if (distance > MIN_DIST) {
 					Log.d(TAG, "compute mid");
-					midPoint(mid, event);
+					computeMidPoint(mid, event);
 					distXY.x = mid.x - BoundingBox.getInstance().getPivot().x;
 					distXY.y = mid.y - BoundingBox.getInstance().getPivot().y;
 					mode = ZOOM;
@@ -345,7 +345,7 @@ public class WalkaRound extends Activity implements HeadUpViewListener,
 				break;
 			case MotionEvent.ACTION_MOVE:
 				if (mode == ZOOM) {
-					float newDist = spacing(event);
+					float newDist = computeDistanceBetweenFingers(event);
 					Log.d(TAG, "newDist=" + newDist);
 					if (newDist > MIN_DIST) {
 						// old zooming computing ... no saving data...
@@ -353,31 +353,41 @@ public class WalkaRound extends Activity implements HeadUpViewListener,
 						float newScale = BoundingBox.getInstance().getScale()
 								+ ((newDist - distance) / ZOOM_PER_PX);
 						Log.d("Scale", "Level A: " + newScale);
+						boolean allowShift = true;
 						if (newScale >= maxZoom) {
-							BoundingBox.getInstance().setLevelOfDetailByADelta(
-									1);
-							newScale = 1;
+							if (BoundingBox.getInstance()
+									.setLevelOfDetailByADelta(1)) {
+								newScale = 1;
+							} else {
+								newScale = maxZoom;
+								allowShift = false;
+							}
 						} else if (newScale <= minZoom) {
-							BoundingBox.getInstance().setLevelOfDetailByADelta(
-									-1);
-							newScale = 1;
+							if (BoundingBox.getInstance()
+									.setLevelOfDetailByADelta(-1)) {
+								newScale = 1;
+							} else {
+								newScale = minZoom;
+								allowShift = false;
+							}
 						}
+
 						Log.d("Scale", "Level S: " + newScale);
 
 						BoundingBox.getInstance().setScale(newScale);
 						// TODO allow scaling to another Point as the center
-						// Point
+						// Point with Pivot Point
 						// BoundingBox.getInstance().setPivot(mid);
 						// float factor = this.spacing(newMid, mid) * 0.1F;
-						
-						float distX = distXY.x * SHIFT_PER_PX;
-						float distY = distXY.y * SHIFT_PER_PX;
-						
-						distXY.x -= distX;
-						distXY.y -= distY;
-						
-						BoundingBox.getInstance().shiftCenter(distX,
-								distY);
+
+						if (allowShift) {
+							float distX = distXY.x * SHIFT_PER_PX;
+							float distY = distXY.y * SHIFT_PER_PX;
+
+							distXY.x -= distX;
+							distXY.y -= distY;
+							BoundingBox.getInstance().shiftCenter(distX, distY);
+						}
 					}
 				}
 				break;
@@ -387,44 +397,27 @@ public class WalkaRound extends Activity implements HeadUpViewListener,
 		}
 
 		/**
+		 * Computes the Distance between two Fingers
 		 * 
 		 * @param event
-		 * @return
+		 *            the needed Notion Event
+		 * @return the distance between Finger one and two
 		 */
-		private float spacing(MotionEvent event) {
+		private float computeDistanceBetweenFingers(MotionEvent event) {
 			float x = event.getX(0) - event.getX(1);
 			float y = event.getY(0) - event.getY(1);
 			return (float) Math.sqrt(x * x + y * y);
 		}
 
 		/**
-		 * 
-		 * @param event
-		 * @return
-		 */
-		private float spacing(PointF p1, PointF p2) {
-			float x = p1.x - p2.x;
-			float y = p1.y - p1.y;
-			return (float) Math.sqrt(x * x + y * y);
-		}
-
-		/**
-		 * 
-		 * @param event
-		 * @return
-		 */
-		private PointF computeCenter(float x, float y) {
-			x = x - BoundingBox.getInstance().getPivot().x;
-			y = y - BoundingBox.getInstance().getPivot().y;
-			return new PointF(x, y);
-		}
-
-		/**
+		 * Computes the mid of two Fingers and saved it in the given Point
 		 * 
 		 * @param point
+		 *            the point which gets the coordinates
 		 * @param event
+		 *            the event with two Fingers
 		 */
-		private void midPoint(PointF point, MotionEvent event) {
+		private void computeMidPoint(PointF point, MotionEvent event) {
 			float x = event.getX(0) + event.getX(1);
 			float y = event.getY(0) + event.getY(1);
 			point.set(x / 2, y / 2);
--- WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/map/BoundingBox.java
+++ WalkaRound/WalkaRound-App/src/edu/kit/iti/algo2/pse2013/walkaround/client/model/map/BoundingBox.java
@@ -15,14 +15,14 @@ import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.Coordinate;
 import edu.kit.iti.algo2.pse2013.walkaround.shared.datastructures.DisplayCoordinate;
 
 /**
- *
+ * 
  * This class represent a rectangle of two Coordinates and his center
  * Coordinate. This class holds the upper Left, bottom Right and center
  * Coordinate.
- *
+ * 
  * @author Ludwig Biermann
  * @version 4.1
- *
+ * 
  */
 public class BoundingBox {
 
@@ -70,7 +70,6 @@ public class BoundingBox {
 	 */
 	private Coordinate scaledBottomRight;
 
-	
 	/**
 	 * Display Size
 	 */
@@ -85,17 +84,17 @@ public class BoundingBox {
 	 * Current copy of LevelOf Detail
 	 */
 	private float levelOfDetail;
-	
+
 	/**
 	 * The Scaling Level
 	 */
 	private float scale;
-	
+
 	/**
 	 * The current Pivot Point
 	 */
 	private PointF pivot;
-	
+
 	/**
 	 * The Level of Detail listener
 	 */
@@ -110,7 +109,6 @@ public class BoundingBox {
 	 * the Scale Listener
 	 */
 	private LinkedList<ScaleListener> scaleListener;
-
 
 	// --------------------------Constructor-------------------------- //
 
@@ -137,7 +135,7 @@ public class BoundingBox {
 
 	/**
 	 * !!!!Only possible if you called initialize or getInstance(Context)
-	 *
+	 * 
 	 * @return BoundingBox
 	 */
 	public static BoundingBox getInstance() {
@@ -146,7 +144,7 @@ public class BoundingBox {
 
 	/**
 	 * Constructs a new Bounding Box
-	 *
+	 * 
 	 * @param center
 	 *            center Coordinate
 	 * @param size
@@ -161,18 +159,18 @@ public class BoundingBox {
 		this.centerListener = new LinkedList<CenterListener>();
 		this.scaleListener = new LinkedList<ScaleListener>();
 		this.display = size;
-		this.levelOfDetail = this.checkLevelOfDetail(levelOfDetail);
+		this.levelOfDetail = this.correctLevelOfDetail(levelOfDetail);
 		this.computeSize();
 		this.setCenter(center, levelOfDetail);
 		this.notifyLODListener(this.levelOfDetail);
-		this.pivot = new PointF(this.display.x/2F, this.display.y/2F);
+		this.pivot = new PointF(this.display.x / 2F, this.display.y / 2F);
 	}
 
 	// --------------------------Setter-------------------------- //
 
 	/**
 	 * Sets a new Center by a new center and a new Level of detail
-	 *
+	 * 
 	 * @param center
 	 *            the center Coordinate
 	 * @param levelOfDetail
@@ -181,7 +179,7 @@ public class BoundingBox {
 	public void setCenter(Coordinate center, float levelOfDetail) {
 		if (this.levelOfDetail != levelOfDetail) {
 			Log.d(TAG, "set a new Level of Detail: " + levelOfDetail);
-			this.levelOfDetail = this.checkLevelOfDetail(levelOfDetail);
+			this.levelOfDetail = this.correctLevelOfDetail(levelOfDetail);
 			this.computeSize();
 		}
 		this.setCenter(center);
@@ -190,7 +188,7 @@ public class BoundingBox {
 
 	/**
 	 * Sets a new Center by a new center and the current Level of Detail
-	 *
+	 * 
 	 * @param center
 	 *            the center Coordinate
 	 */
@@ -202,11 +200,12 @@ public class BoundingBox {
 		this.bottomRight = this.computeBottomRight();
 		this.notifyCenterListener(center);
 	}
-	
+
 	/**
 	 * Set a new scale.
 	 * 
-	 * @param scale the new Scale
+	 * @param scale
+	 *            the new Scale
 	 */
 	public void setScale(float scale) {
 		this.scale = scale;
@@ -215,7 +214,7 @@ public class BoundingBox {
 
 	/**
 	 * Sets a new Center to the given DisplayCoordinate
-	 *
+	 * 
 	 * @param dc
 	 *            the given Display Coordinate
 	 */
@@ -234,7 +233,7 @@ public class BoundingBox {
 
 	/**
 	 * Shifts the Center Coordinate bei a Pixel delta
-	 *
+	 * 
 	 * @param x
 	 *            pixel delta
 	 * @param y
@@ -256,13 +255,34 @@ public class BoundingBox {
 	}
 
 	/**
+	 * Checks the correctness of level of Detail
+	 * 
+	 * @param levelOfDetail
+	 *            the new level of detail;
+	 * @return true of the level of detail is correct returns false if the level
+	 *         of detail is out of bound
+	 */
+	public boolean checkLevelOfDetail(float levelOfDetail) {
+		if (CurrentMapStyleModel.getInstance().getCurrentMapStyle()
+				.getMaxLevelOfDetail() <= levelOfDetail) {
+			return false;
+		}
+
+		if (CurrentMapStyleModel.getInstance().getCurrentMapStyle()
+				.getMinLevelOfDetail() >= levelOfDetail) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
 	 * Sets a new Level of detail by checking the Boundings
-	 *
+	 * 
 	 * @param levelOfDetail
 	 *            the new level of detail;
 	 * @return the correct new LOD
 	 */
-	private float checkLevelOfDetail(float levelOfDetail) {
+	private float correctLevelOfDetail(float levelOfDetail) {
 		if (CurrentMapStyleModel.getInstance().getCurrentMapStyle()
 				.getMaxLevelOfDetail() <= levelOfDetail) {
 			return CurrentMapStyleModel.getInstance().getCurrentMapStyle()
@@ -279,31 +299,36 @@ public class BoundingBox {
 
 	/**
 	 * Sets a new Level Of Detail by a delta
-	 *
+	 * 
 	 * @param delta
 	 *            the delta to the new Level Of Detail
+	 * @returns false if the Level of details has to fitted into its bounds.
 	 */
-	public void setLevelOfDetailByADelta(float delta) {
+	public boolean setLevelOfDetailByADelta(float delta) {
+		boolean check = this.checkLevelOfDetail(levelOfDetail + delta);
 		this.setLevelOfDetail(levelOfDetail + delta);
+		return check;
 	}
 
 	/**
 	 * Sets a new Level of Detail
-	 *
+	 * 
 	 * @param levelOfDetail
 	 *            the new Level of Detail
 	 */
 	public void setLevelOfDetail(float levelOfDetail) {
-		this.levelOfDetail = this.checkLevelOfDetail(levelOfDetail);
+		this.levelOfDetail = this.correctLevelOfDetail(levelOfDetail);
 		this.computeSize();
 		this.topLeft = this.computeTopLeft();
 		this.bottomRight = this.computeBottomRight();
 		this.notifyLODListener(this.levelOfDetail);
 	}
-	
+
 	/**
 	 * Sets a new Pivot Point.
-	 * @param pivot the new Pivot Point
+	 * 
+	 * @param pivot
+	 *            the new Pivot Point
 	 */
 	public void setPivot(PointF pivot) {
 		this.pivot = pivot;
@@ -316,33 +341,31 @@ public class BoundingBox {
 	 * 
 	 * @return scaling levle
 	 */
-	public float getScale(){
+	public float getScale() {
 		return scale;
 	}
-	
+
 	/**
 	 * Gives back the Coordinate of the upper left corner
-	 *
+	 * 
 	 * @return top left
 	 */
 	public Coordinate getTopLeft() {
 		return topLeft;
 	}
-	
 
 	/**
 	 * Gives back the Coordinate of the upper left corner
-	 *
+	 * 
 	 * @return top left
 	 */
 	public Coordinate getScaledTopLeft() {
 		return computeScaledTopLeft();
 	}
-	
 
 	/**
 	 * Gives back the Coordinate of the upper right corner
-	 *
+	 * 
 	 * @return top right
 	 */
 	public Coordinate getTopRight() {
@@ -351,7 +374,7 @@ public class BoundingBox {
 
 	/**
 	 * Gives back the Coordinate of the upper right corner
-	 *
+	 * 
 	 * @return bottom left
 	 */
 	public Coordinate getBottomLeft() {
@@ -360,16 +383,16 @@ public class BoundingBox {
 
 	/**
 	 * Gives back the Coordinate of the upper right corner
-	 *
+	 * 
 	 * @return bottom right
 	 */
 	public Coordinate getScaledBottomRight() {
 		return computeScaledBottomRight();
 	}
-	
+
 	/**
 	 * Gives back the Coordinate of the upper right corner
-	 *
+	 * 
 	 * @return bottom right
 	 */
 	public Coordinate getBottomRight() {
@@ -378,7 +401,7 @@ public class BoundingBox {
 
 	/**
 	 * Gives back the Center Coordinate of the box
-	 *
+	 * 
 	 * @return the coordinate at the center of the screen
 	 */
 	public Coordinate getCenter() {
@@ -387,7 +410,7 @@ public class BoundingBox {
 
 	/**
 	 * Gives the width and the height of the current Display back
-	 *
+	 * 
 	 * @return display size as Point
 	 */
 	public Point getDisplaySize() {
@@ -396,15 +419,16 @@ public class BoundingBox {
 
 	/**
 	 * Gives the current Level Of Detail back
-	 *
+	 * 
 	 * @return Level of Detail as float
 	 */
 	public float getLevelOfDetail() {
 		return levelOfDetail;
 	}
-	
+
 	/**
 	 * Gives the current PivotPoint back
+	 * 
 	 * @return the Pivot Point
 	 */
 	public PointF getPivot() {
@@ -415,7 +439,7 @@ public class BoundingBox {
 
 	/**
 	 * Compute the Size of the Display
-	 *
+	 * 
 	 * @param levelOfDetail
 	 */
 	private void computeSize() {
@@ -430,19 +454,20 @@ public class BoundingBox {
 				CoordinateUtility.convertPixelsToDegrees(display.y,
 						levelOfDetail, CoordinateUtility.DIRECTION_LATITUDE));
 	}
+
 	/**
 	 * Returns the scaled upperLeft Coordinate
-	 *
+	 * 
 	 * @return the scaled top left geo-oordinate
 	 */
 	private Coordinate computeScaledTopLeft() {
 		Log.d(TAG, "compute Scaled Top Left");
 		return this.topLeft;
 	}
-	
+
 	/**
 	 * Returns the upperLeft Coordinate
-	 *
+	 * 
 	 * @return the top left geo-oordinate
 	 */
 	private Coordinate computeTopLeft() {
@@ -452,7 +477,7 @@ public class BoundingBox {
 
 	/**
 	 * Returns the upper right Coordinate
-	 *
+	 * 
 	 * @return the top right geo-oordinate
 	 */
 	private Coordinate computeTopRight() {
@@ -462,7 +487,7 @@ public class BoundingBox {
 
 	/**
 	 * Returns the bootom left Coordinate
-	 *
+	 * 
 	 * @return the bottom left geo-oordinate
 	 */
 	private Coordinate computeBottomLeft() {
@@ -472,17 +497,17 @@ public class BoundingBox {
 
 	/**
 	 * Returns the scaled bottom right Coordinate
-	 *
+	 * 
 	 * @return the scaled bottom right geo-oordinate
 	 */
 	private Coordinate computeScaledBottomRight() {
 		Log.d(TAG, "Compute BottomRight");
-		return  this.bottomRight;
+		return this.bottomRight;
 	}
-	
+
 	/**
 	 * Returns the bottom right Coordinate
-	 *
+	 * 
 	 * @return the bottom right geo-oordinate
 	 */
 	private Coordinate computeBottomRight() {
@@ -504,9 +529,9 @@ public class BoundingBox {
 	/**
 	 * A simple Helper Class to pair two doubles. In this Case the make
 	 * relationship between width and height of the display in Coordinates
-	 *
+	 * 
 	 * @author Ludwig Biermann
-	 *
+	 * 
 	 */
 	private class DoublePairing {
 
@@ -522,7 +547,7 @@ public class BoundingBox {
 
 		/**
 		 * Construct a new Double Paring
-		 *
+		 * 
 		 * @param width
 		 *            of the display
 		 * @param height
@@ -539,21 +564,20 @@ public class BoundingBox {
 		}
 	}
 
-
 	// --------------------------Listener-------------------------- //
 
 	/**
 	 * A Interface to notify classes if the Level of Detail changes
-	 *
+	 * 
 	 * @author Ludwig Biermann
 	 * @version 1.0
-	 *
+	 * 
 	 */
 	public interface LevelOfDetailListener {
 
 		/**
 		 * If the level of Detail changes, this class will be called.
-		 *
+		 * 
 		 * @param levelOfDetail
 		 */
 		public void onLevelOfDetailChange(float levelOfDetail);
@@ -562,7 +586,7 @@ public class BoundingBox {
 
 	/**
 	 * This will register a new Level Of Detail Listener
-	 *
+	 * 
 	 * @param listener
 	 *            the new listener
 	 * @return always true
@@ -575,7 +599,7 @@ public class BoundingBox {
 
 	/**
 	 * This method will notify all Level of Detail Listener
-	 *
+	 * 
 	 * @param levelOfDetail
 	 *            the new level of Detail
 	 */
@@ -588,7 +612,7 @@ public class BoundingBox {
 
 	/**
 	 * A Interface to notify classes if the Center Coordinate changes
-	 *
+	 * 
 	 * @author Ludwig Biermann
 	 * @version 1.0
 	 */
@@ -596,7 +620,10 @@ public class BoundingBox {
 
 		/**
 		 * If the Center Coordinate changes, this class will be called.
-		 * @param center the new Coordinate that is currently at the center of the screen
+		 * 
+		 * @param center
+		 *            the new Coordinate that is currently at the center of the
+		 *            screen
 		 */
 		public void onCenterChange(Coordinate center);
 
@@ -604,68 +631,72 @@ public class BoundingBox {
 
 	/**
 	 * This will register a new Center Coordinate Listener
-	 *
+	 * 
 	 * @param listener
 	 *            the new Listener
 	 * @return the current Center Coordinate
 	 */
 	public Coordinate registerCenterListener(CenterListener listener) {
-		//Log.d(TAG, "register a new LOD Listener: " + listener.toString());
+		// Log.d(TAG, "register a new LOD Listener: " + listener.toString());
 		this.centerListener.add(listener);
 		return this.center;
 	}
 
 	/**
 	 * This will notify all Center Listener
-	 *
+	 * 
 	 * @param center
 	 *            the new Center Coordiante
 	 */
 	private void notifyCenterListener(Coordinate center) {
-		//Log.d(TAG, "notify Center Listener new Center: " + center.toString());
+		// Log.d(TAG, "notify Center Listener new Center: " +
+		// center.toString());
 		for (CenterListener l : this.centerListener) {
 			l.onCenterChange(center);
 		}
 	}
+
 	/**
 	 * This will register a new Center Coordinate Listener
-	 *
+	 * 
 	 * @param listener
 	 *            the new Listener
 	 * @return the current Center Coordinate
 	 */
 	public float registerScaleListener(ScaleListener listener) {
-		//Log.d(TAG, "register a new LOD Listener: " + listener.toString());
+		// Log.d(TAG, "register a new LOD Listener: " + listener.toString());
 		this.scaleListener.add(listener);
 		return this.scale;
 	}
-	
+
 	/**
 	 * This will notify all Scale Listener
-	 *
+	 * 
 	 * @param center
 	 *            the new Center Coordiante
 	 */
 	private void notifyScaleListener(float scale) {
-		//Log.d(TAG, "notify Center Listener new Center: " + center.toString());
+		// Log.d(TAG, "notify Center Listener new Center: " +
+		// center.toString());
 		for (ScaleListener l : this.scaleListener) {
 			l.onScaleChange(scale);
 		}
 	}
-	
+
 	/**
 	 * A Listener wich is called if the scale level is changed
 	 * 
 	 * @author Ludwig Biermann
 	 * @version 1.0
-	 *
+	 * 
 	 */
-	public interface ScaleListener{
-		
+	public interface ScaleListener {
+
 		/**
 		 * Is called if the scale level is changed
 		 * 
-		 * @param scale the new scale level
+		 * @param scale
+		 *            the new scale level
 		 */
 		public void onScaleChange(float scale);
 	}
